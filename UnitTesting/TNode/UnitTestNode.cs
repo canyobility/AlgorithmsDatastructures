66be9f97e1d1ad783e729a6dcfdf96fa0e22824f
canyobility <rigginsgavin@gmail.com>
2026-09-21 12:07:53 -0600

    'Smart' node connection system V1

--- UnitTesting/TNode/UnitTestNode.cs
+++ UnitTesting/TNode/UnitTestNode.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Net.Sockets;
 using System.Text;
 
 using Source.Utility.GenericNode;
+using Shouldly;
 
 namespace UnitTesting.TNode
 {
@@ -12,16 +15,23 @@ namespace UnitTesting.TNode
         public void CreateGenericNode()
         {
             Node<int> node1 = new Node<int>(5);
-            Node<string> node2 = new Node<string>("Test");
+            Node<string> node2 = new Node<string>();
             node1.value = 1;
             node2.value = "cat";
+
+            
         }
 
         [Fact]
-        public void Test()
+        public void ConnectNode()
         {
+            Node<string> node1 = new Node<string>();
+            Node<string> node2 = new Node<string>();
 
+            //NodeSocket<string> socket1 = node1.AddSocket();
+            NodeConnection<string> c = node1.Connect(node2, 5);
+
+            node1.GetAtSocket(0).Target.ShouldBe(node2);
         }
-        
     }
 }
